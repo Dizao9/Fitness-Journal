a2af19d5d32214b08af024d282c75258863abd7e
Dizao <Maks.cherty@yandex.ru>
2026-03-06 16:15:15 +0300

    feat: implement get function for exercises page

--- internal/storage/exercise.go
+++ internal/storage/exercise.go
@@ -1,9 +1,12 @@
 package storage
 
 import (
+	"context"
 	"database/sql"
+	"log"
 
 	"github.com/Dizao9/Fitness-Journal/internal/domain"
+	"github.com/google/uuid"
 )
 
 type ExerciseStorage struct {
@@ -24,4 +27,28 @@ func (s *ExerciseStorage) CreateExercise(exercise domain.Exercise) (int, error) 
 		return 0, err
 	}
 	return id, nil
+}
+
+func (s *ExerciseStorage) GetExercises(ctx context.Context, userID uuid.UUID, limit, offset int) ([]domain.Exercise, error) {
+	rows, err := s.DB.Query(`SELECT * FROM exercises WHERE
+	athlete_id IS NULL
+	OR
+	athlete_id = $1
+	ORDER BY name ASC
+	LIMIT $2 OFFSET $3`, userID, limit, offset)
+	if err != nil {
+		return nil, err
+	}
+
+	exercises := make([]domain.Exercise, 0, 50)
+	for rows.Next() {
+		var exercise domain.Exercise
+		err = rows.Scan(&exercise.ID, &exercise.Name, &exercise.MuscleGroup, &exercise.Description, &exercise.AthleteID)
+		exercises = append(exercises, exercise)
+	}
+	if err != nil {
+		log.Printf("[GET_EXERCISES_STORAGE] error: %v", err)
+	}
+
+	return exercises, nil
 }
